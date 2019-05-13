7032e748cbb921ec09715659d7a474f8d0bdf558
CW Wolf <44934371+ethancampbell77@users.noreply.github.com>
2019-05-13 07:41:15 -0400

    2nd commit - getting there

---
+++ app/data/friends.js
@@ -0,0 +1,85 @@
+var friends = [
+    {
+        name: "Bob",
+        photo: "../images/bobSaget.jpg",
+        scores: [
+            "2",
+            "3",
+            "1",
+            "5",
+            "1",
+            "1",
+            "4",
+            "1",
+            "4",
+            "4"
+        ]
+    },
+    {
+        name: "Rick",
+        photo: "../images/rickSanchez.jpg",
+        scores: [
+            "4",
+            "1",
+            "1",
+            "1",
+            "2",
+            "3",
+            "2",
+            "5",
+            "5",
+            "5"
+        ]
+    },
+    {
+        name: "Glenn",
+        photo: "../images/glennDanzig.jpg",
+        scores: [
+            "5",
+            "5",
+            "2",
+            "5",
+            "1",
+            "5",
+            "5",
+            "1",
+            "2",
+            "3"
+        ]
+    },
+    {
+        name: "Bruce",
+        photo: "../images/bruceCampbell.jpg",
+        scores: [
+            "3",
+            "5",
+            "1",
+            "2",
+            "2",
+            "2",
+            "3",
+            "5",
+            "4",
+            "2"
+        ]
+    },
+    {
+        name: "Artie",
+        photo: "../images/artieLang.jpg",
+        scores: [
+            "1",
+            "5",
+            "5",
+            "2",
+            "3",
+            "1",
+            "2",
+            "2",
+            "5",
+            "1"
+        ]
+    },
+
+];
+
+module.exports = friends;
